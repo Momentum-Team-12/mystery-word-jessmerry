4d036aae40018eaac60d2c9d720af1534f5ba607
Jess Merry <merryja.email@gmail.com>
2022-04-27 15:32:49 -0400

    level of difficulty

--- mystery_word.py
+++ mystery_word.py
@@ -1,12 +1,56 @@
 import string
 import random
+import sys
+
+
+def get_list():
+
+    EASY_WORDS = []
+    MEDIUM_WORDS = []
+    HARD_WORDS = []
+
+    with open('words.txt') as file_contents:
+        contents_string = file_contents.read()
+        contents_list = contents_string.split()
+
+    for word in contents_list:
+        if 4 <= len(word) <= 6:
+            EASY_WORDS.append(word)
+        elif 7 <= len(word) <= 8:
+            MEDIUM_WORDS.append(word)
+        elif len(word) >= 9:
+            HARD_WORDS.append(word)
+
+    level = input("Choose your difficulty: E, M, D : ")
+    if level == "E":
+        return EASY_WORDS
+    elif level == "M":
+        return MEDIUM_WORDS
+    elif level == "D":
+        return HARD_WORDS
+
+
+def play_again():
+    print("Game over, Would you like to play again? y/n")
+    play_response = input()
+    yes_input = ['y', 'Y']
+    no_input = ['n', 'N']
+    if play_response in yes_input:
+        play_game()
+    elif play_response in no_input:
+        exit()
+    else:
+        print("Invalid response. Enter y for new game, n to exit.")
+        play_again()
+    exit()
 
 
 def play_game():
 
-    with open('words.txt') as file_contents:
-        contents_string = file_contents.read()
-    contents_list = contents_string.split()
+#    with open('words.txt') as file_contents:
+#        contents_string = file_contents.read()
+#    contents_list = contents_string.split()
+    contents_list = get_list()
     random_word = random.choice(contents_list)
 #    print(random_word)
     print(f'I am thinking of a word. It has {len(random_word)} letters...')
@@ -22,14 +66,15 @@ def play_game():
                 print(letter, end=' ')
 
             else:
-                print("_")
+                print("_", end=" ")
 #                print(letter, end=' ')
                 failed += 1
 
         if failed == 0:
             print ("You Win")
             print ("The word is: ", random_word)
-            break
+#            break
+            play_again()
 
         print()
         guess = input("guess a character:")
@@ -42,6 +87,7 @@ def play_game():
 
             if turns == 0:
                 print("You Lose, the word is: ", random_word)
+                play_again()
 
 
 if __name__ == "__main__":
